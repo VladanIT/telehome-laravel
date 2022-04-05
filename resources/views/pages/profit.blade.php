0343ea644434da77b3eda49e3cd41c2b5232f00f
Vladan Matorkić <vladan.matorkic.96@gmail.com>
2022-04-06 00:41:19 +0200

    Profit section
    Creating profit page

---
+++ resources/views/pages/profit.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.index')
+
+@section('title')
+    Zarada
+@endsection
+
+@section('content')
+<div class="content-label">
+    <h1>Zarada za trenutni mesec</h1>
+
+
+</div>
+@endsection
